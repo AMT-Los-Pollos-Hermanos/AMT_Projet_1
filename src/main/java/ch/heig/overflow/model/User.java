196c537a4c3a1291ab677d7791542dd7d3ec76da
Julien Béguin <julien.beguin99@gmail.com>
2020-09-19 18:21:38 +0200

    add user model

--- src/main/java/ch/heig/overflow/model/User.java
+++ src/main/java/ch/heig/overflow/model/User.java
@@ -1,4 +1,18 @@
 package ch.heig.overflow.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class User {
+    @Getter
+    @Setter
+    private int id;
+
+    @Getter
+    @Setter
+    private String username;
+
+    @Getter
+    @Setter
+    private String password;
 }
