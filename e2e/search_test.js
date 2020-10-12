882b894c616caad947ac5ccaef94c52bce4d38b0
Gil Balsiger <gil.balsiger@gmail.com>
2020-10-12 22:59:22 +0200

    Add e2e test for searching

---
+++ e2e/search_test.js
@@ -0,0 +1,25 @@
+Feature('search');
+
+Scenario('test search title', ({ I }) => {
+    I.amOnPage('/')
+    I.fillField('s', 'question 2')
+    I.click('Search')
+    I.see('Question 2')
+    I.see('Contenu 2')
+    I.dontSee('Question 1')
+    I.dontSee('Contenu 1')
+    I.dontSee('Question 3')
+    I.dontSee('Contenu 3')
+});
+
+Scenario('test search author', ({ I }) => {
+    I.amOnPage('/')
+    I.fillField('s', 'gil')
+    I.click('Search')
+    I.see('Question 1')
+    I.see('Contenu 1')
+    I.dontSee('Question 2')
+    I.dontSee('Contenu 2')
+    I.dontSee('Question 3')
+    I.dontSee('Contenu 3')
+});
