bd048b1ec6425747f8c6ef00a28c593050ea4a4c
Gaetan <gaetan.daubresse@gmail.com>
2020-10-12 19:04:30 +0200

    add dependencies on pom.xml and created structure of AuthFacadeTestIT

---
+++ src/test/java/ch/heig/amt/overflow/application/auth/AuthFacadeTestIT.java
@@ -0,0 +1,29 @@
+package ch.heig.amt.overflow.application.auth;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(Arquillian.class)
+public class AuthFacadeTestIT {
+
+    private final static String WARNAME = "arquillian-managed.war";
+
+    @Inject
+    AuthFacade authFacade;
+
+    @Deployment(testable = true)
+    public static WebArchive createDeployment() {
+        WebArchive archive = ShrinkWrap.create(WebArchive.class, WARNAME)
+                .addPackages(true, "ch.heigvd.amt");
+        return archive;
+    }
+}
