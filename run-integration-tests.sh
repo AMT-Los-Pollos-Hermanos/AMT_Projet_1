ccb5f6d9e4c9f14d86d64b8b8a23d276ecdcbc2d
Gil Balsiger <gil.balsiger@gmail.com>
2020-10-14 11:12:01 +0200

    IT 2.0

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-mvn liberty:stop
-
 mvn clean package
 mvn liberty:create liberty:install-feature liberty:deploy
-mvn liberty:start
-#mvn liberty:configure-arquillian
+mvn liberty:configure-arquillian
 
-mvn verify
-#mvn liberty:stop
+mvn failsafe:integration-test
+
+mvn liberty:stop
--- src/test/java/ch/heig/amt/overflow/application/auth/AuthFacadeTestIT.java
+++ src/test/java/ch/heig/amt/overflow/application/auth/AuthFacadeTestIT.java
@@ -1,19 +1,16 @@
 package ch.heig.amt.overflow.application.auth;
 
-import ch.heig.amt.overflow.domain.user.IUserRepository;
 import ch.heig.amt.overflow.infrastructure.persistence.jdbc.JdbcUserRepository;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @RunWith(Arquillian.class)
@@ -21,34 +18,32 @@ public class AuthFacadeTestIT {
 
     private final static String WARNAME = "arquillian-managed.war";
 
-//    @Inject
-//    JdbcUserRepository userRepository;
-//
-//    AuthFacade facade;
+    @Inject
+    JdbcUserRepository userRepository;
 
-//    @BeforeEach
-//    void setUp() {
-//        facade = new AuthFacade(userRepository);
-//    }
+    AuthFacade facade;
+
+    @Before
+    public void setUp() {
+        facade = new AuthFacade(userRepository);
+    }
 
     @Deployment(testable = true)
     public static WebArchive createDeployment() {
         WebArchive archive = ShrinkWrap.create(WebArchive.class, WARNAME)
-                .addPackages(true, "ch.heigvd.amt");
+                .addPackages(true, "ch.heig.amt");
         return archive;
     }
 
     @Test
     public void testRegister(){
-//        RegisterCommand cmd = RegisterCommand.builder()
-//                .firstName("John")
-//                .lastName("Doe")
-//                .email("john.doe@heig.ch")
-//                .username("johndoe")
-//                .clearTextPassword("1234")
-//                .build();
-//        assertDoesNotThrow(() -> facade.register(cmd));
-        int a = 1;
-        assertEquals(a, 1);
+        RegisterCommand cmd = RegisterCommand.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@heig.ch")
+                .username("johndoe")
+                .clearTextPassword("1234")
+                .build();
+        assertDoesNotThrow(() -> facade.register(cmd));
     }
 }
