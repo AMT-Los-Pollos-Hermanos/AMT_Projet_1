ec7000032d98f1cba580016aa2ad723a2415dba5
Julien Béguin <julien.beguin99@gmail.com>
2020-10-19 19:00:40 +0200

    add comment DTO

---
+++ src/main/java/ch/heig/amt/overflow/application/comment/CommentsDTO.java
@@ -0,0 +1,40 @@
+package ch.heig.amt.overflow.application.comment;
+
+import ch.heig.amt.overflow.application.auth.UserDTO;
+import ch.heig.amt.overflow.domain.MainContentId;
+import lombok.Builder;
+import lombok.Singular;
+import lombok.Value;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+@Builder
+@Value
+public class CommentsDTO {
+
+    @Builder
+    @Value
+    public static class CommentDTO {
+        MainContentId mainContentId;
+        String content;
+        Date createdAt;
+        UserDTO author;
+
+        public String formattedCreatedAt() {
+            DateFormat format = new SimpleDateFormat("'Le 'dd.MM.yyyy' à 'HH:mm:ss");
+            format.setTimeZone(TimeZone.getTimeZone("Europe/Zurich"));
+            return format.format(createdAt);
+        }
+
+    }
+
+
+
+    @Singular
+    List<CommentDTO> comments;
+
+}
