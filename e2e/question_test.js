dd622db15cbfab1175ca4d228d9554ec4befcdf0
Gaetan <gaetan.daubresse@gmail.com>
2020-10-21 16:18:47 +0200

    add test when user is not logged

--- e2e/question_test.js
+++ e2e/question_test.js
@@ -1,10 +1,12 @@
 Feature('question');
 
-Before(({login}) => {
-    login('user');
+Scenario('test add question with unlogged user', ({ I }) => {
+    I.amOnPage('/questions')
+    I.amOnPage('/login')
 });
 
-Scenario('test add question', ({ I }) => {
+Scenario('test add question', ({I, login}) => {
+    login('user')
     I.amOnPage('/questions')
     I.fillField('Titre', 'Quelle est le sens de la vie ?')
     I.fillField('.ck.ck-content.ck-editor__editable', '42')
@@ -12,13 +14,4 @@ Scenario('test add question', ({ I }) => {
     I.seeInCurrentUrl('/questions')
     I.see('Quelle est le sens de la vie ?', '.card-header')
     I.see('42', '.card-body')
-});
-
-Scenario('test add question failed', ({ I }) => {
-    I.amOnPage('/questions')
-    I.fillField('Titre', '')
-    I.fillField('Contenu', '')
-    I.click('Ajouter')
-    I.seeInCurrentUrl('/questions')
-    I.see('Une erreur s\'est produite lors de l\'ajout de votre question')
 });
