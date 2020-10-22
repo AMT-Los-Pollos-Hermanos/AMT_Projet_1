09e246518416e98d17ca262f71a6596afad649ea
Gil Balsiger <gil.balsiger@gmail.com>
2020-10-22 15:14:26 +0200

    Fix e2e

--- e2e/answer_test.js
+++ e2e/answer_test.js
@@ -1,26 +1,26 @@
 Feature('answer');
 
-Scenario('Answer with unlogged user', ({ I }) => {
+Scenario('Answer with logged out user', ({ I }) => {
     I.amOnPage('/question/9603ce27-77c2-43c1-8c58-e01ca3c07242')
     //Redirected when user not connected
-    I.amOnPage('/login')
+    I.seeInCurrentUrl('/login')
 });
 
-Scenario('Answer with logged user', ( {I, login} ) => {
-    login('user');
-    I.amOnPage('/question/6ec53ae2-1a39-4ab5-8291-bc664051b855')
+Scenario('Answer with logged in user', ( {I, login} ) => {
+    login('user')
+    I.amOnPage('/question/18dbc27f-d54f-417c-b576-07f1c3cfd301')
     I.fillField('Rich Text Editor, main', 'tralalilalaaa')
     I.click('Ajouter')
-    I.seeInCurrentUrl('/question/6ec53ae2-1a39-4ab5-8291-bc664051b855')
+    I.seeInCurrentUrl('/question/18dbc27f-d54f-417c-b576-07f1c3cfd301')
     I.see('tralalilalaaa')
 });
 
 Scenario('Answer with logged user', ( {I, login} ) => {
     login('user');
-    I.amOnPage('/question/9603ce27-77c2-43c1-8c58-e01ca3c07242')
+    I.amOnPage('/question/58dbc27f-d54f-417c-b576-07f1c3cfd301')
     I.fillField('Rich Text Editor, main', 'I dont understand this question')
     I.click('Ajouter')
-    I.seeInCurrentUrl('/question/9603ce27-77c2-43c1-8c58-e01ca3c07242')
+    I.seeInCurrentUrl('/question/58dbc27f-d54f-417c-b576-07f1c3cfd301')
     I.see('I dont understand this question')
 });
 
